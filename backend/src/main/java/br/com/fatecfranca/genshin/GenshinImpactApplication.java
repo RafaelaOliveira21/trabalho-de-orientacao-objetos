bc67ed46c7171e0a4881d455c9ce5f28937abb0c
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-06-07 19:40:29 -0300

    Add backend

---
+++ backend/src/main/java/br/com/fatecfranca/genshin/GenshinImpactApplication.java
@@ -0,0 +1,13 @@
+package br.com.fatecfranca.genshin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenshinImpactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenshinImpactApplication.class, args);
+	}
+
+}
